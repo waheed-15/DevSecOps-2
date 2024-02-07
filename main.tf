37391009683a65c6d2de5e4f8db36bead78c6719
waheed <zamawaheed1506@gmail.com>
2024-02-07 16:38:22 +0530

    waheed

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = var.region
+}
+
+module "ecr" {
+  region = var.region
+  source                 = "./modules/ecr-repository"   # Assuming ecr-module is the directory where your ECR module is located
+  ecr_repository_name    = var.ecr_repository_name
+}
+
+module "eks" {
+  region = var.region
+  source                 = "./modules/eks-cluster"   # Assuming eks-module is the directory where your EKS module is located
+  eks_role_name          = var.eks_role_name
+  cluster_name           = var.cluster_name
+  subnet_ids             = var.subnet_ids
+  security_group_ids     = var.security_group_ids
+  endpoint_public_access = var.endpoint_public_access
+  endpoint_private_access= var.endpoint_private_access
+  node_role_name         = var.node_role_name
+  node_group_name        = var.node_group_name
+  desired_capacity       = var.desired_capacity
+  min_size               = var.min_size
+  max_size               = var.max_size
+  instance_type          = var.instance_type
+  tags = var.tags
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
+
+output "eks_node_role_arn" {
+  value = module.eks.node_role_arn
+}
+
+output "eks_node_group_name" {
+  value = module.eks.node_group_name
+}
